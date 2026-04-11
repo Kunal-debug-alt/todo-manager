3b711c1bb710d9bddaedc22b0b54b0f51454e483
KUNAL <kunalgajbe2005@gmail.com>
2026-04-12 00:03:07 +0530

    Fix asgi app import order for Django settings

--- todo_project/asgi.py
+++ todo_project/asgi.py
@@ -8,18 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
+
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import todo_project.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
-
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter(
 	{
 		'http': django_asgi_app,
